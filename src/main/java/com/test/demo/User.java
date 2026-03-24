e41ee257978173d4e831b35b30d38e2811a05dc9
tanaykulkarni27 <1002kulkarnitp@gmail.com>
2026-03-24 23:34:55 +0530

    init

--- src/main/java/com/test/demo/User.java
+++ src/main/java/com/test/demo/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name="users")
-public class User{
+class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
--- src/main/java/com/test/demo/UserRepository.java
+++ src/main/java/com/test/demo/UserRepository.java
@@ -5,10 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-
+// Long : Data type of your @Id field [primary key]
+// User : ClassName of the Entity
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "select * from users",nativeQuery = true)
-    List<User> getAll();
     @Query(value = "select u.password from User u where u.name = :name")
     String getHashedPassword(@Param("name") String name);
 }
